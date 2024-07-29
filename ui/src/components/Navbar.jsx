12d66d80509745de4c238066777f0f58be78a34f
Prem <prempravinsapkale@gmail.com>
2024-07-29 07:54:14 +0530

    navbar with theme toggle

---
+++ ui/src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "../providers/themeProvider";
+
+export const Navbar = () => {
+   const { theme, setTheme } = useContext(ThemeContext);
+   const [show, setShow] = useState(false);
+   const [lastScrollY, setLastScrollY] = useState(0);
+
+   function toggleTheme() {
+      if (theme === "light") {
+         document.body.classList.add("dark-theme");
+         setTheme("dark");
+      } else {
+         document.body.classList.remove("dark-theme");
+         setTheme("light");
+      }
+   }
+
+   useEffect(() => {
+      const handleScroll = () => {
+         const currentScrollY = window.scrollY;
+
+         if (currentScrollY < lastScrollY) {
+            setShow(false);
+         } else {
+            setShow(true);
+         }
+
+         setLastScrollY(currentScrollY);
+      };
+
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+         window.removeEventListener("scroll", handleScroll);
+      };
+   }, [lastScrollY]);
+
+   return (
+      <div
+         id="navbar"
+         className={`bg-black duration-300 ${
+            show ? "-translate-y-full" : "translate-y-0"
+         } w-full text-white flex items-center justify-between py-2 px-10 sticky top-0`}
+      >
+         <a href="/" className="text-[16px] font-[1000] cursor-pointer">
+            Blog.dev
+         </a>
+         <div className="text-[12px] font-[100] flex gap-10 items-center justify-between">
+            <div className="cursor-pointer">Topic</div>
+            <button className="cursor-pointer" onClick={toggleTheme}>
+               <img
+                  src="/button_dark-mode-new.svg"
+                  alt=""
+                  height="26"
+                  width="26"
+               />
+            </button>
+            <div className="cursor-pointer">
+               <img alt="" height="16" src="/search.svg" width="16" />
+            </div>
+         </div>
+      </div>
+   );
+};
---
+++ ui/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {},
+      fontFamily: {
+         Sharp23: "Sharp Grotesk PE Trial Book 23",
+         SharpBold23: "Sharp Grotesk PE Trial Bold 23",
+         Sharp19: "Sharp Grotesk PE Trial Book 19",
+      },
+   },
+   plugins: [],
+};

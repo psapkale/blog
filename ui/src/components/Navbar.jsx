0ea2cfdf3bcbf90665afa5570e1d198997bdb9b1
Prem <prempravinsapkale@gmail.com>
2024-08-31 10:07:32 +0530

    topics in navbar

--- ui/src/components/Navbar.jsx
+++ ui/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ export const Navbar = () => {
    const [show, setShow] = useState(false);
    const [lastScrollY, setLastScrollY] = useState(0);
    const { isLogin, setIsLogin } = useContext(LoginContext);
+   const [showTopics, setShowTopics] = useState(false);
 
    function toggleTheme() {
       if (theme === "light") {
@@ -54,7 +55,58 @@ export const Navbar = () => {
             Blog.dev
          </a>
          <div className="text-[12px] font-[100] flex gap-10 items-center justify-between">
-            <div className="cursor-pointer">Topic</div>
+            <div onMouseEnter={() => setShowTopics(true)} className="relative">
+               <div className="cursor-pointer p-2 peer">Topic</div>
+               {showTopics && (
+                  <div
+                     onMouseLeave={() => setShowTopics(false)}
+                     className="bg-black absolute top-[38px] -left-[80px] px-8 py-4 text-[16px] text-nowrap flex flex-col gap-2 transition-opacity duration-500"
+                  >
+                     <a
+                        href="/Application"
+                        className="text-[#b4dc11] hover:underline cursor-pointer"
+                     >
+                        Application
+                     </a>
+                     <a
+                        href="/FrontEnd"
+                        className="text-[#14c8eb] hover:underline cursor-pointer"
+                     >
+                        Front End
+                     </a>
+                     <a
+                        href="/Infrastructure"
+                        className="text-[#c8aff0] hover:underline cursor-pointer"
+                     >
+                        Infrastructure
+                     </a>
+                     <a
+                        href="/MachineLearning"
+                        className="text-[#ffaf95] hover:underline cursor-pointer"
+                     >
+                        Machine Learning
+                     </a>
+                     <a
+                        href="/Mobile"
+                        className="text-[#ff8c19] hover:underline cursor-pointer"
+                     >
+                        Mobile
+                     </a>
+                     <a
+                        href="/Security"
+                        className="text-[#b4c8e1] hover:underline cursor-pointer"
+                     >
+                        Security
+                     </a>
+                     <a
+                        href="/Culture"
+                        className="text-[#fa551e] hover:underline cursor-pointer"
+                     >
+                        Culture
+                     </a>
+                  </div>
+               )}
+            </div>
             {isLogin ? (
                <>
                   <a href="/create" className="cursor-pointer hover:underline">
